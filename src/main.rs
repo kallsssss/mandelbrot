41cbcab6e2e5b258c4ec09acdd6740309834f1d1
kallsssss <kaarlokzop@proton.me>
2026-05-31 15:35:59 +0200

    modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
 fn factorial(k: i32) -> i32 {
-    if k != 0 {
-        k * factorial(k - 1)
-    } else {
-        1 // base case: 0! = 1
-    }
+    if k != 0 { k * factorial(k - 1) } else { 1 }
 }
 
 fn main() {
-    print!("{}", factorial(10)); // 3628800
+    print!("{}", factorial(10));
 }
